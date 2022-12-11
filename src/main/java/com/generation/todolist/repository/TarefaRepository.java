9d4520249dab17da9b3f6dfe178e0afa9120ce4c
Brenda Bispo <115106207+brendabispo@users.noreply.github.com>
2022-12-11 20:24:53 -0300

    adicioando testes Tarefa

--- src/main/java/com/generation/todolist/repository/TarefaRepository.java
+++ src/main/java/com/generation/todolist/repository/TarefaRepository.java
@@ -1,6 +1,9 @@
 package com.generation.todolist.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.todolist.model.Tarefa;
@@ -8,4 +11,5 @@ import com.generation.todolist.model.Tarefa;
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long>{
 
+	public List <Tarefa> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
